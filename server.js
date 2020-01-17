87ba67f03bd7915f9ff9fc82d4ca53ad69743787
Bridget Asser <bridgetasser@gmail.com>
2020-01-17 10:34:45 -0500

    fs to output JSON file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var cheerio = require("cheerio");
 var app = express();
 
 app.get("/scrape", function(req, res) {
-  url = "http://www.imdb.com/title/tt1229340/";
+  url = "https://www.imdb.com/title/tt0098627/";
 
   request(url, function(error, response, html) {
     if (!error) {
@@ -20,7 +20,6 @@ app.get("/scrape", function(req, res) {
           .children()
           .first()
           .text();
-
         release = data
           .children()
           .last()
@@ -33,14 +32,26 @@ app.get("/scrape", function(req, res) {
 
       $(".star-box-giga-star").filter(function() {
         var data = $(this);
-
-        //  no need to traverse the DOM any further
-
         rating = data.text();
 
         json.rating = rating;
       });
     }
+
+    // To write to the system we will use the built in 'fs' library.
+    // In this example we will pass 3 parameters to the writeFile function
+    // Parameter 1 :  output.json - this is what the created filename will be called
+    // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+    // Parameter 3 :  callback function - a callback function to let us know the status of our function
+
+    fs.writeFile("output.json", JSON.stringify(json, null, 4), function(err) {
+      console.log(
+        "File successfully written! - Check your project directory for the output.json file"
+      );
+    });
+
+    // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+    res.send("Check console");
   });
 });
 
